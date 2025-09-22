79508f6089ab4495fa848bda7703d15aad2658c9
Percy Liang <percyliang@gmail.com>
2025-09-21 18:05:08 -0700

    update welcome lecture

--- welcome.py
+++ welcome.py
@@ -5,19 +5,19 @@ def main():
     image("images/course-staff.png", width=600)
 
     what_is_ai()
-    what_is_this_program()  # Interlude
-    course_philosophy()
-    course_logistics()
+    about_this_course()
+    what_is_this_program()
 
 
 def what_is_ai():
-    text("We have lots of examples of AI")
-    text("- AI assistants (ChatGPT, Claude, Gemini, Grok)")
-    text("- Autonomous vehicles (Waymo, Wayve)")
-    text("- Game playing (Deep Blue, AlphaGo, AlphaStar)")
-    text("- 3D protein structure prediction (AlphaFold)")
+    text("We have lots of examples of AI all around us:")
+    text("- AI assistants: ChatGPT, Claude, Gemini, Grok")
+    text("- Autonomous vehicles: Waymo, Wayve")
+    text("- Game playing: [Deep Blue](https://en.wikipedia.org/wiki/Deep_Blue_(chess_computer)), [AlphaGo](https://en.wikipedia.org/wiki/AlphaGo), [AlphaStar](https://en.wikipedia.org/wiki/AlphaStar_(software))")
+    text("- Competition math and programming: [IMO](https://x.com/OpenAI/status/1946594928945148246), [IOI](https://codeforces.com/blog/entry/134091), [ICPC](https://venturebeat.com/ai/google-and-openais-coding-wins-at-university-competition-show-enterprise-ai)")
+    text("- 3D protein structure prediction: [AlphaFold](https://deepmind.google/science/alphafold/)")
 
-    text("But what is AI?")
+    text("But what exactly **is** AI?")
 
     text("Artificial intelligence")
     text("- artificial: runs on a computer (or robot)")
@@ -26,7 +26,10 @@ def what_is_ai():
     text("We could define **intelligence** in terms of humans...")
     text("...but we seek a definition from general principles.")
 
-    text("## Ingredients of intelligence")
+    text("### Ingredients of intelligence")
+    text("What kinds of things should an intelligent agent be able to do?")
+    image("images/perceive-reason-act-learn.png", width=600)
+    image("images/self-driving-image.png", width=400)
 
     text("**Perceive**: process raw inputs from the world")
     text("- visual scene understanding")
@@ -34,69 +37,71 @@ def what_is_ai():
     text("- natural language understanding")
 
     text("**Reason**: use knowledge + percepts to draw inferences about the world")
-    text("- uniform cost search (in deterministic world)")
+    text("- uniform cost search (in a deterministic world)")
     text("- value iteration (decision-making under uncertainty)")
     text("- minimax (for adversial games)")
     text("- probabilistic inference (in Bayesian networks)")
 
-    text("**Act**: do stuff to the world")
+    text("**Act**: output actions that affect the world")
     text("- Text/image generation")
     text("- Speech synthesis")
     text("- Robot manipulation")
 
-    text("**Learn**: update state based on experience")
+    text("**Learn**: update agent based on experience")
     text("- Gradient descent")
     text("- Q-learning (reinforcement learning)")
-    text("- Expectation Maximization (for Bayesian networks)")
+    text("- Expectation maximization (for Bayesian networks)")
 
     text("...all under **resource constraints**")
-    text("- Computation: running time of algorithm, memory, communication")
-    text("- Information: data, inputs available")
+    text("- Computation: running time (also: memory, communication)")
+    text("- Information: data / experience, available inputs in a given situation")
 
-    text("So what does it do?")
-    text("- AI implicitly or explicitly encodes goals / objectives / values / utility functions")
-    text("- Alignment: how does these values correspond to what humans want?")
-    text("- This is a deep, sociotechnical problem")
+    text("### Goals")
 
-    text("## Summary")
+    text("But what does the **developer** want the agent to achieve?")
+    text("- An agent explicitly or implicitly encodes values / goals / objectives / utility functions")
+    text("- Alignment: how do make these values correspond to what the developer wants?")
+    text("- Example: ChatGPT aims to be informative, avoid hallucinations, refuse harmful queries")
+
+    text("Also what impact **we** want the agent to have on society?")
+    text("- Issues: privacy, copyright, jobs, inequality, geopolitics")
+    text("- This is a deep, **sociotechnical** problem (who's we?)")
+    text("- Fundamental tradeoffs between people's values")
+    text("- Unintended consequences (social media, impact on education)")
+
+    text("### Summary")
     text("- Ingredients of intelligence: perception, reasoning, action, learning")
-    text("- Resource constraints: computation, information")
-    text("- Alignment: important for AI to actually benefit society")
+    text("- Resource constraints: computation, information"), note("So we'll want to develop algorithms that are compute and data efficient.")
+    text("- Developer goals: how do we build AI to accomplish something?")
+    text("- Societal goals: how can we develop AI that benefits society?")
 
 
-def course_philosophy():
+def about_this_course():
     text("Course philosophy")
     text("- Timeless foundations (e.g., gradient descent)")
     text("- Modern examples (e.g., GPT-5 solving 12/12 problems at the ICPC)")
-    text("- Code-driven: most precise, need it anyway at the end of the day")
+    text("- Learn by doing: grounded in building practical applications")
 
     text("Changes this year:")
     text("- Tensor-native: from deep learning to value iteration to Bayesian network inference")
     text("- Cut constraint satisfaction problems :(")
-    text("- Deeper dive into societal issues (e.g., copyright)")
+    text("- Deep dive into societal impact (e.g., copyright, supply chains, policy)")
+
+    text("All course policies, coursework, and schedule are online:")
+    link("https://stanford-cs221.github.io/autumn2025/")
 
 
 def what_is_this_program():
     text("This is an *executable lecture*, a program whose execution delivers the content of a lecture.")
-    text("Executable lectures make it possible to:")
-    text("- view and run code (since everything is code!),")
+    
+    text("We can step through code:")
     total = 0  # @inspect total
     for x in [1, 2, 3]:  # @inspect x
         total += x  # @inspect total
-    text("- see the hierarchical structure of the lecture, and")
-    text("- jump to definitions and concepts: "), link(what_is_ai)
 
-
-def course_logistics():
-    text("All information online: "), link("https://stanford-cs221.github.io/autumn2025/")
-
-    text("## Lectures")
-    text("## Assignments")
-    text("## Exam")
-    text("## Project (optional)")
-
-    text("## Office hours")
-    text("## Honor code")
+    text("- Lectures inherit the hierarchical structure of code")
+    text("- Code is more precise (than English and also than math)")
+    text("- Need to write code to build AI at the end of the day")
 
 
 if __name__ == "__main__":
